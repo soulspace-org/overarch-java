d05ac2c29e5351d6be362b2652ffcd2fb72f77d4
Ludger Solbach <soulman@soulspace.org>
2024-12-07 13:29:47 +0100

    updated project files

--- README.md
+++ README.md
@@ -3,6 +3,16 @@ Overarch Java provides an annotation processor for the java compiler to generate
 
 *draft*
 
+![GitHub](https://img.shields.io/github/license/soulspace-org/overarch-java)
+
+## Build
+Currently [leiningen](https://leiningen.org) is used to build the `overarch-java.jar`.
+
+For a local build and installation in the maven repository use
+```
+lein install
+```
+
 ## Usage
 Add the `overarch-java` and `overarch-java-annotation` dependencies to your project
 and annotate your java sources with the overarch annotations.
--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject org.soulspace/overarch-java "0.1.0-SNAPSHOT"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.soulspace/overarch-java-annotations "0.1.0-SNAPSHOT"]]
   ;:java-source-paths ["src/main/java"]
@@ -6,4 +9,6 @@
   :resource-paths ["resources"]
   :aot [org.soulspace.overarch.java.annotation-processor]
   ;:main ^:skip-aot org.soulspace.overarch.java.annotation-processor
-  )
+
+  :scm {:name "git" :url "https://github.com/soulspace-org/overarch-java"}
+  :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]])
